e97b86d3877222afcd105b0bc53d1c36f44af727
Taras Serdyuk <taras.serdyuk@gmail.com>
2012-10-28 05:05:35 +0200

    detailed test failure messages

--- test/Test.hs
+++ test/Test.hs
@@ -1,13 +1,17 @@
 
 module Test where
 
-import Control.Monad
-import Language.Haskell.Interpreter hiding (parens)
+import Control.Monad.Error
+import Language.Haskell.Interpreter
 import Type.Tuple.Test.Phantom
 
 
 main :: IO ()
-main = runInterpreter tests >>= print
+main = runInterpreter tests >>= putStrLn . result where
+    result res = case res of
+        Left (NotAllowed err) -> err
+        Left _ -> "Oops!"
+        Right _ -> "Ok"
 
 
 tests :: Interpreter ()
@@ -24,7 +28,13 @@ check2 :: (Functor m, MonadInterpreter m) => String -> String -> String -> m ()
 check2 cl a = check cl [a]
 
 check :: (Functor m, MonadInterpreter m) => String -> [String] -> String -> m ()
-check cl pars res = do
-    let expr = applyClass cl pars res
-    correct <- typeChecks expr
-    unless correct (void . eval . showPhantom $ expr)
+check cl pars res = catchError (compile expr) rethrow where
+    expr = applyClass cl pars res
+    compile = void . eval . showPhantom
+    rethrow err = throwError $ NotAllowed (inst ++ showError err)
+    inst = unwords (cl : pars ++ [res, "\n"])
+    showError err = case err of
+        WontCompile msgs -> unlines $ map errMsg msgs
+        GhcException msg -> msg
+        UnknownError msg -> msg
+        NotAllowed msg -> msg
