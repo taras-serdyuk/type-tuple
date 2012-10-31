3981fe23fbdd4b41790ef53de924f4ae6a52de45
Taras Serdyuk <taras.serdyuk@gmail.com>
2012-10-31 17:55:31 +0200

    refactoring

--- test/Type/Tuple/Test/Expression.hs
+++ test/Type/Tuple/Test/Expression.hs
@@ -1,11 +1,22 @@
 
-module Type.Tuple.Test.Phantom where
+module Type.Tuple.Test.Expression where
+-- TODO: legacy
 
 import Type.Tuple.Test.Text
 
 
+mkInst :: String -> [String] -> String -> String
+mkInst cl pars res = unwords (cl : pars ++ [res])
+
+-- TODO: refactor
+applyInst :: String -> String
+applyInst inst = truePhantom $ applyClass cl pars res where
+    (cl : rest) = words inst
+    (pars, res) = (init rest, last rest)
+
 truePhantom :: String2
 truePhantom expr = "const True" .- parens expr
+
 
 applyClass :: String -> [String] -> String2
 applyClass cl pars res = unwords (func : map phantom pars) .:: res
--- test/Type/Tuple/Test/Test.hs
+++ test/Type/Tuple/Test/Test.hs
@@ -6,7 +6,7 @@ import Control.Monad -- TODO: delete
 import Language.Haskell.Interpreter -- TODO: delete
 import Type.Tuple.Test.Data
 import Type.Tuple.Test.Interpreter
-import Type.Tuple.Test.Phantom
+import Type.Tuple.Test.Expression
 
 
 type Class = String
@@ -17,25 +17,17 @@ is, no :: String -> Interpreter ()
 is inst = valid inst (applyInst inst)
 no inst = invalid inst (applyInst inst)
 
--- TODO: refactor
-applyInst :: String -> String
-applyInst inst = truePhantom $ applyClass cl pars res where
-    (cl : rest) = words inst
-    (pars, res) = (init rest, last rest)
-
 
 -- TODO: convert to class For
 for1 :: (RenderType a, RenderType r, DataGenerator g a) => (Int, Class, a -> r) -> g -> Interpreter ()
 for1 (n, cl, et) gen = apply1 (mapM_ (\x -> validInterpInstR cl [x] (et x))) (applyGen n gen)
 
 for2 :: (DataGenerator g1 a, DataGenerator g2 b, RenderType a, RenderType b, RenderType r) => (Int, Class, a -> b -> r) -> (g1, g2) -> Interpreter ()
-for2 (n, cl, et) (xGen, yGen) = apply2 (zipWithM_ (\x y -> valid (inst cl [renderType x, renderType y] (renderType $ et x y)) . truePhantom $ applyClass cl [renderType x, renderType y] (renderType $ et x y))) (applyGen n xGen) (applyGen n yGen)
+for2 (n, cl, et) (xGen, yGen) = apply2 (zipWithM_ (\x y -> valid (mkInst cl [renderType x, renderType y] (renderType $ et x y)) . truePhantom $ applyClass cl [renderType x, renderType y] (renderType $ et x y))) (applyGen n xGen) (applyGen n yGen)
 
-inst :: String -> [String] -> String -> String
-inst cl pars res = unwords (cl : pars ++ [res])
 
 validInterpInstR :: (RenderType a, RenderType b) => String -> [a] -> b -> Interpreter ()
-validInterpInstR cl pars res = valid (inst cl (map renderType pars) (renderType res)) . truePhantom $ applyClass cl (map renderType pars) (renderType res)
+validInterpInstR cl pars res = valid (mkInst cl (map renderType pars) (renderType res)) . truePhantom $ applyClass cl (map renderType pars) (renderType res)
 
 apply1 :: (MonadIO m) => (a -> m b) -> IO a -> m b
 apply1 f x = liftIO x >>= f
